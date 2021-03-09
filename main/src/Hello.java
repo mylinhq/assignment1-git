54f3df9783876279f500b28c5f2415714a3e1325
mylinhq <79225395+mylinhq@users.noreply.github.com>
2021-03-09 02:29:06 -0800

    Added String variable pussInBoots with the value of meow

--- main/src/Hello.java
+++ main/src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
     public static void main(String[] args){
         String shrek = "rOaR";
+        String pussInBoots = "meow";
         System.out.println("do the roar");
         for(int i = 0; i < 4; i++){
             System.out.println(shrek);
