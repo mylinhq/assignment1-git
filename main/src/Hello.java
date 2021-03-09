95ecd4ac5c0339e5692822ef31bb319579c7f6b7
mylinhq <79225395+mylinhq@users.noreply.github.com>
2021-03-09 02:35:42 -0800

    added gingy string variable

--- main/src/Hello.java
+++ main/src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args){
         String shrek = "rOaR";
         String pussInBoots = "meow";
+        String gingy = "do u know the muffin man?";
         System.out.println("do the roar");
         for(int i = 0; i < 4; i++){
             System.out.println(shrek);
