d3b2f7fd2828a2f87b42c2e77ba5ad074c28e2cf
mylinhq <79225395+mylinhq@users.noreply.github.com>
2021-03-09 13:51:48 -0800

    readding Hello.java

--- main/src/Hello.java
+++ main/src/Hello.java
@@ -3,7 +3,11 @@ public class Hello {
     public static void main(String[] args){
         String shrek = "rOaR";
         String pussInBoots = "meow";
+
         String gingy = "do u know the muffin man?";
+
+        int zero = 0;
+
         System.out.println("do the roar");
         for(int i = 0; i < 4; i++){
             System.out.println(shrek);
