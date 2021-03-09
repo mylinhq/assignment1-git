eba80fd9956261f8dd289207e2eba3b90134264b
mylinhq <79225395+mylinhq@users.noreply.github.com>
2021-03-09 01:56:37 -0800

    Added for loop that iterates 4 times with the print statement rOaR.

--- main/src/Hello.java
+++ main/src/Hello.java
@@ -1,6 +1,10 @@
 
 public class Hello {
     public static void main(String[] args){
+        String shrek = "rOaR";
         System.out.println("do the roar");
+        for(int i = 0; i < 4; i++){
+            System.out.println(shrek);
+        }
     }
 }
